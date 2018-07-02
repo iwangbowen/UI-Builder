21db4a7900cfd8e7c7e0561d03abab9cd531a257
Wang Bowen <iwangbowen@gmail.com>
2018-07-02 14:53:57 +0800

    format some code

--- src/builder.js
+++ src/builder.js
@@ -72,7 +72,9 @@ function removeUnusedTags(html) {
 	const el = document.createElement('html');
 	el.innerHTML = html;
 
-	removeTag(el, 'hr', tag => $(tag).hasClass('horizontal-line') || $(tag).hasClass('vertical-line'));
+	removeTag(el, 'hr',
+		tag => $(tag).hasClass('horizontal-line')
+			|| $(tag).hasClass('vertical-line'));
 	removeTag(el, 'link',
 		tag => tag.getAttribute('rel') == 'stylesheet'
 			&& !tag.getAttribute('href').includes('bootstrap'));
