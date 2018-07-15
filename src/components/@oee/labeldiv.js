3f51361614f79f7d71b4f0561a94f38ac759fb9a
Wang Bowen <iwangbowen@gmail.com>
2018-07-15 08:51:38 +0800

    Change label text in tool bar

--- src/components/@oee/labeldiv.js
+++ src/components/@oee/labeldiv.js
@@ -1,9 +1,11 @@
+import { dataComponentId } from '../common';
+
 const labeldiv = {
     name: 'Label Div',
     image: 'icons/label.svg',
     html: `<div class="everyOutbox-left draggable">
                 <i class="fa fa-caret-square-o-right text-danger" aria-hidden="true"></i>
-                <span class="theme">Period</span>
+                <span ${dataComponentId}="html/span@oee" class="theme">Period</span>
            </div>`
 };
 
