81ce827bd9ebda6cb392f00fdb050904d58d34c5
Bowen Wang <iwangbowen@gmail.com>
2018-08-02 10:36:26 +0800

    Removed unnecessary files

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('hello world')});
-app.listen(8200, () => console.log('listening on port 8200'));
