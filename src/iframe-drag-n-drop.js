5b2f0760445c1edc67e30ff45a03868b004e5c61
Bowen Wang <iwangbowen@gmail.com>
2018-08-21 15:57:22 +0800

    Make specified divs unresizable

--- src/iframe-drag-n-drop.js
+++ src/iframe-drag-n-drop.js
@@ -70,37 +70,30 @@ $(document).ready(() => {
         })
         .resizable({
             // resize from all edges and corners
-            edges: { left: true, right: true, bottom: true, top: true },
-
+            edges: { left: false, right: false, bottom: false, top: false },
             // keep the edges inside the parent
             restrictEdges: {
                 outer: 'parent',
                 endOnly: true,
             },
-
             // minimum size
             restrictSize: {
                 min: { width: 100, height: 50 },
             },
-
             inertia: true,
         })
         .on('resizemove', function (event) {
             var target = event.target,
                 x = (parseFloat(target.getAttribute('data-x')) || 0),
                 y = (parseFloat(target.getAttribute('data-y')) || 0);
-
             // update the element's style
             target.style.width = event.rect.width + 'px';
             target.style.height = event.rect.height + 'px';
-
             // translate when resizing from top or left edges
             x += event.deltaRect.left;
             y += event.deltaRect.top;
-
             target.style.webkitTransform = target.style.transform =
                 'translate(' + x + 'px,' + y + 'px)';
-
             target.setAttribute('data-x', x);
             target.setAttribute('data-y', y);
         });
