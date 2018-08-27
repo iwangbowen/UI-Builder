93dbb5f7939a07cb8bb12ff07c96f59720adf53f
Bowen Wang <iwangbowen@gmail.com>
2018-08-27 23:38:43 +0800

    Fix downloading file issue in ie

--- src/util/dom.js
+++ src/util/dom.js
@@ -65,21 +65,23 @@ function launchFullScreen(document) {
 }
 
 function downloadAsTextFile(filename, text) {
-    const element = document.createElement('a');
-    if (text.length < 2 * 1024 * 1024) {
-        element.setAttribute('href', `data:text/html;charset=utf-8,${encodeURIComponent(text)}`);
+    if (window.navigator.msSaveBlob) { // IE hack; see http://msdn.microsoft.com/en-us/library/ie/hh779016.aspx
+        const blob = new Blob([text], { type: 'text/html' });
+        window.navigator.msSaveOrOpenBlob(blob, filename);
     } else {
-        const blob = new Blob([text]);
-        element.setAttribute('href', URL.createObjectURL(blob));
+        const element = document.createElement('a');
+        if (text.length < 2 * 1024 * 1024) {
+            element.setAttribute('href', `data:text/html;charset=utf-8,${encodeURIComponent(text)}`);
+        } else {
+            const blob = new Blob([text], { type: 'text/html' });
+            element.setAttribute('href', URL.createObjectURL(blob));
+        }
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     }
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
 }
 
 function getDoctype(doc) {
