1ebea1adf2316392eebc1ecad865fec72112af2d
Bowen Wang <iwangbowen@gmail.com>
2018-09-10 17:22:43 +0800

    Fix multi-value select bug

--- src/components/@oee/multivalueselect.js
+++ src/components/@oee/multivalueselect.js
@@ -4,7 +4,7 @@ import { dataComponentId, dataMultivalueSelectId } from '../common';
 
 const multivalueselect = $.extend({}, autoselectinput, {
     name: "Multi-value Select",
-    html: `<div class="everyOutbox-right draggable">
+    html: `<div class="everyOutbox-right draggable" ${dataComponentId}="${multivalueselectid}">
             <div class="btn-group">
                 <div class="dailyBox">
                     <select ${dataMultivalueSelectId} ${dataComponentId}="${multivalueselectid}" class="form-control fundodooSelect js-example-basic-multiple" multiple="multiple" lustyle="height: 2.8rem;width:13rem">
