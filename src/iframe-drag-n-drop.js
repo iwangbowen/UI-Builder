2b34b6425dfe1aff8f5a51a59b30933d3d8d4d5d
Bowen Wang <iwangbowen@gmail.com>
2018-09-13 00:37:14 +0800

    Try TODO Highlight extension

--- src/iframe-drag-n-drop.js
+++ src/iframe-drag-n-drop.js
@@ -3,6 +3,12 @@ import {
 } from './util/iframe-drag-n-drop-util';
 import { addData, editData, deleteData, getAddContent, getEditContent } from './layer';
 
+/**
+ * TODO:
+ * - Implement element drag inside iframe
+ * - Use jQuery-ui instead of interact.js
+ * - Remove code inside iframe to parent page
+ */
 $(document).ready(() => {
     self.interact = interact;
     self.arrowKeyMove = arrowKeyMove;
