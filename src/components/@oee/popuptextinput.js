ea1661b3c0c03230fb6a2f722b26dcb540272a8a
Bowen Wang <iwangbowen@gmail.com>
2018-09-19 11:51:07 +0800

    Refactor popup text input component code

--- src/components/@oee/popuptextinput.js
+++ src/components/@oee/popuptextinput.js
@@ -1,19 +1,14 @@
-import { inputTypeNames } from '../inputTypes';
 import { dataComponentId, sortableClass } from '../common';
 import { popuptextinputid } from './ids';
-import input from './input';
-import { properties } from '../textinput';
+import textinput from './textinput';
 
-const popuptextinput = $.extend({}, input, {
+const popuptextinput = $.extend({}, textinput, {
     name: 'Popup Text Input',
-    attributes: { type: inputTypeNames },
-    image: 'icons/text_input.svg',
     sortable: true,
     html: `<div class="everyInput ${sortableClass}" ${dataComponentId}="${popuptextinputid}">
             <div class="inputText">Label</div>
             <input class="addInput" type="text" onfocus="this.removeAttribute('readonly')" value="" autocomplete="off">
-           </div>`,
-    properties
+           </div>`
 });
 
 export default popuptextinput;
