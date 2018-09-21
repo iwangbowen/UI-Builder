0ca3ed3bb92b4b7f1c1e4a1b100170070f419ebd
Bowen Wang <iwangbowen@gmail.com>
2018-09-21 13:25:30 +0800

    Change build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,65 +37,17 @@ module.exports = function (grunt) {
                 }
             },
         },
-        babel: {
-            options: {
-                sourceMap: true,
-                presets: ['env']
-            },
-            transpile: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'lib/',
-                        src: ['*.js'],
-                        dest: 'transpiled/',
-                        ext: '.babel.js'
-                    }
-                ]
-            },
-            build: {
-                files: {
-                    'dist/built.js': 'concated/concated.js',
-                    'dist/iframe-drag-n-drop.js': 'src/iframe-drag-n-drop.js'
-                }
-            }
-        },
-        concat: {
-            es6: {
-                src: [
-                    'transpiled/builder*.js',
-                    'transpiled/undo*.js',
-                    'transpiled/inputs*.js',
-                    'transpiled/components-bootstrap4*.js',
-                    'transpiled/drag-n-drop*.js'
-                ],
-                dest: 'dist/built.js'
-            },
-            es5: {
-                src: [
-                    // 'lib/builder*.js',
-                    'lib/undo*.js',
-                    'lib/drag-n-drop*.js'
-                ],
-                dest: 'concated/concated.js'
-            }
-        },
         clean: {
-            concated: ['concated'],
-            built: ['dist'],
-            transpiled: ['transpiled']
+            built: ['dist']
         }
     });
 
     // Load plugins
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-browserify');
 
     // Register tasks
     grunt.registerTask("build:dev", ['clean:all', 'browserify:dev']);
     grunt.registerTask("build:prod", ['clean:all', 'browserify:prod']);
-    grunt.registerTask('build', ['clean:all', 'concat:es5', 'babel:build']);
-    grunt.registerTask('transpile', ['babel:transpile']);
-    grunt.registerTask('clean:all', ['clean:concated', 'clean:built', 'clean:transpiled']);
+    grunt.registerTask('clean:all', ['clean:built']);
 };
