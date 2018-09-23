5f10e3e7904b32c5ab67e2b75ff879804fd3927f
Bowen Wang <iwangbowen@gmail.com>
2018-09-23 09:30:28 +0800

    Remove TODO comments

--- src/iframe.js
+++ src/iframe.js
@@ -2,12 +2,6 @@ import {
     initDropzone, initResizeDrag, initDraggable, setGlobalVariables
 } from './util/iframe-drag-n-drop';
 
-/**
- * TODO:
- * - Implement element drag inside iframe
- * - Use jQuery-ui instead of interact.js
- * - Remove code inside iframe to parent page
- */
 $(document).ready(() => {
     setGlobalVariables();
     initDropzone();
