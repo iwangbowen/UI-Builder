5ac2bfd803db1576ad61ce2e78f1ea9b7be68f28
Bowen Wang <iwangbowen@gmail.com>
2018-10-11 19:02:52 +0800

    Change y dimension size

--- src/util/grid.js
+++ src/util/grid.js
@@ -3,7 +3,7 @@ const isInBuilder = true;
 function grid() {
     const gridster = $(".gridster ul").gridster({
         widget_margins: [5, 5],
-        widget_base_dimensions: ['auto', 140],
+        widget_base_dimensions: ['auto', 50],
         autogrow_cols: true,
         max_cols: 12,
         resize: {
