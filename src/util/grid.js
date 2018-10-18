6bb76c0f688b069ba4c64dd14cd548eca1f91563
Bowen Wang <iwangbowen@gmail.com>
2018-10-18 15:17:25 +0800

    Check if $.fn.gridster exists in non-grid html

--- src/util/grid.js
+++ src/util/grid.js
@@ -7,36 +7,38 @@ function grid() {
                 .find('#select-box, #highlight-box, #highlight-name').hide();
         }
     }
-    var gridster = $(".gridster").gridster({
-        widget_selector: 'div.gridster > div',
-        widget_margins: [5, 5],
-        widget_base_dimensions: ['auto', 50],
-        shift_widgets_up: false,
-        shift_larger_widgets_down: false,
-        max_cols: 12,
-        max_rows: 60,
-        resize: {
-            enabled: isInBuilder,
-            start: hideToolBoxes
-        },
-        draggable: {
-            start: hideToolBoxes,
-            // exclude form children elements to prevent intervention with jquery-ui sortable
-            handle: '*:not(form *)'
-        }
-    }).data('gridster');
-    if (isInBuilder) {
-        window.parent.enableSortableAndDroppable($('div.gridster > div > form'));
-        $('div.gridster > div').each(function () {
-            if (!$(this).has('span.gs-remove-handle').length) {
-                $('<span class="gs-remove-handle"></span>').appendTo(this);
+    if ($('.gridster').length) {
+        var gridster = $(".gridster").gridster({
+            widget_selector: 'div.gridster > div',
+            widget_margins: [5, 5],
+            widget_base_dimensions: ['auto', 50],
+            shift_widgets_up: false,
+            shift_larger_widgets_down: false,
+            max_cols: 12,
+            max_rows: 60,
+            resize: {
+                enabled: isInBuilder,
+                start: hideToolBoxes
+            },
+            draggable: {
+                start: hideToolBoxes,
+                // exclude form children elements to prevent intervention with jquery-ui sortable
+                handle: '*:not(form *)'
             }
-        });
-        $('div.gridster').on('click', 'div > span.gs-remove-handle', function () {
-            gridster.remove_widget($(this).parent());
-        });
-    } else {
-        gridster.disable();
+        }).data('gridster');
+        if (isInBuilder) {
+            window.parent.enableSortableAndDroppable($('div.gridster > div > form'));
+            $('div.gridster > div').each(function () {
+                if (!$(this).has('span.gs-remove-handle').length) {
+                    $('<span class="gs-remove-handle"></span>').appendTo(this);
+                }
+            });
+            $('div.gridster').on('click', 'div > span.gs-remove-handle', function () {
+                gridster.remove_widget($(this).parent());
+            });
+        } else {
+            gridster.disable();
+        }
     }
     $('.grid-footer button').click(function () {
         var addedWidget = gridster.add_widget('<div><span class="gs-remove-handle"></span></div>', 3, 3);
