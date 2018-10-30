3f366bbc7090ef2e1564f3fdc9acdece7c0c38c0
Bowen Wang <iwangbowen@gmail.com>
2018-10-30 15:52:46 +0800

    Fix input label and help text delete

--- src/components/@oee/bootstraptextinputfield.js
+++ src/components/@oee/bootstraptextinputfield.js
@@ -1,15 +1,15 @@
-import { dataComponentId, sortableClass, formGroup, formText, textMuted } from '../common';
+import { dataComponentId, sortableClass, formGroup, formText, textMuted, deletableComponent } from '../common';
 import { bootstraptextinputfieldid } from './ids';
 import bootstraptextinput from './bootstraptextinput';
 import _ from 'lodash';
 import textinputfield from './textinputfield';
-import label from '../@general/label';
+import label from './label';
 
 const bootstraptextinputfield = _.extend({}, textinputfield, {
     html: `<div class="${formGroup} ${sortableClass}" ${dataComponentId}="${bootstraptextinputfieldid}">
             ${label.html}
             ${bootstraptextinput.html}
-            <small class="${formText} ${textMuted}">We'll never share your email.</small>
+            <small class="${formText} ${textMuted} ${deletableComponent}">We'll never share your email.</small>
            </div>`
 });
 
