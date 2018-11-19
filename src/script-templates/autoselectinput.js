7e8a4b8762e63afa708d5db91ffb2c5d7517719c
Bowen Wang <iwangbowen@gmail.com>
2018-11-19 15:32:56 +0800

    Work around pds issue for demo purpose

--- src/script-templates/autoselectinput.js
+++ src/script-templates/autoselectinput.js
@@ -17,8 +17,17 @@ function template() {
             .filter(function (el) {
                 return $(el).attr('${dataUrl}');
             }).forEach(function (el) {
+                var data = {
+                    owner: 'Common',
+                    queryId: 'findIdType',
+                    version: '10001',
+                    paramSource: null
+                };
                 $.ajax({
                     url: config.fundodooApiDomainUrl + $(el).attr('${dataUrl}'),
+                    dataType: 'json',
+                    method: 'POST',
+                    data: data,
                     async: true,
                     success: function (response) {
                         generateOptions(el, response.data);
