5ecc964cda69f2ba9ffb372ee82de371d75e16c1
Bowen Wang <iwangbowen@gmail.com>
2018-12-06 14:57:05 +0800

    Fix button-group delete bug

--- src/components/@common/bootstrapbuttongroup.js
+++ src/components/@common/bootstrapbuttongroup.js
@@ -2,12 +2,13 @@ import { buttonGroupProperties as properties } from '../properties/button';
 import button from './button';
 import extend from 'lodash/extend';
 import basiccomponent from './basiccomponent';
+import { configurableComponent } from '../common';
 
 const bootstrapbuttongroup = extend({}, basiccomponent, {
     classes: ["btn-group"],
     name: "Button Group",
     image: "icons/button_group.svg",
-    html: `<div class="btn-group" role="group" aria-label="Basic example">
+    html: `<div class="btn-group ${configurableComponent}" role="group" aria-label="Basic example">
                 ${button.html}
                 ${button.html}
                 ${button.html}
