2faef902e8284da6c2eec96f5e066f55ca62f695
Bowen Wang <iwangbowen@gmail.com>
2018-12-21 13:50:46 +0800

    Change button default size

--- src/components/@common/button.js
+++ src/components/@common/button.js
@@ -8,7 +8,7 @@ const button = {
     name: "Button",
     image: "icons/button.svg",
     sortable: true,
-    html: `<button ${dataComponentId}=${buttonid} type="button" class="btn btn-primary ${cloneableComponent}">Search</button>`,
+    html: `<button ${dataComponentId}=${buttonid} type="button" class="btn btn-primary btn-sm ${cloneableComponent}">Search</button>`,
     properties
 };
 
