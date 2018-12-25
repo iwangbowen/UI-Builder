94fc5e51742607899ac92c7d2d99f3f40d48317d
Bowen Wang <iwangbowen@gmail.com>
2018-12-25 10:42:48 +0800

    Add dashed border for form

--- src/components/@common/form.js
+++ src/components/@common/form.js
@@ -1,5 +1,5 @@
 import { formProperties as properties } from '../properties/form';
-import { configurableComponent, dashBorderClass, dataComponentId } from '../common';
+import { configurableComponent, dataComponentId, placeholderComponentHighlight } from '../common';
 import { formid } from './ids';
 
 const form = {
@@ -7,7 +7,7 @@ const form = {
     image: "icons/form.svg",
     name: "Form",
     dragHtml: `<img ${dataComponentId}="${formid}" src="libs/builder/icons/form.svg" style="width: 100px; height: auto;">`,
-    html: `<form class="${configurableComponent}" ${dataComponentId}="${formid}">
+    html: `<form class="${configurableComponent} ${placeholderComponentHighlight}" ${dataComponentId}="${formid}">
            </form>`,
     getDropHtml() {
         return this.html;
