a3daf6509173eb04a7582db5a61fa78e41ab0d0d
Bowen Wang <iwangbowen@gmail.com>
2018-12-25 13:39:06 +0800

    Change hr drag html

--- src/components/@common/bootstraphr.js
+++ src/components/@common/bootstraphr.js
@@ -1,12 +1,17 @@
 import basiccomponent from './basiccomponent';
 import extend from 'lodash/extend';
-import { cloneableComponent } from '../common';
+import { cloneableComponent, dataComponentId } from '../common';
+import { bootstraphrid } from './ids';
 
 const bootstraphr = extend({}, basiccomponent, {
     image: "icons/hr.svg",
     nodes: ["hr"],
     name: "Horizontal Rule",
-    html: `<hr class="${cloneableComponent}">`
+    dragHtml: `<img ${dataComponentId}="${bootstraphrid}" src="libs/builder/icons/hr.svg" style="width: 100px; height: auto;">`,
+    html: `<hr class="${cloneableComponent}">`,
+    getDropHtml() {
+        return this.html;
+    }
 });
 
 export default bootstraphr;
