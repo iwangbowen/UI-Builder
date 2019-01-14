98504e1d0bb2b08139d4931864e1995a32fe457a
Bowen Wang <iwangbowen@gmail.com>
2019-01-14 12:13:08 +0800

    Fix table undraggable after refresh

--- src/components/@common/commontable.js
+++ src/components/@common/commontable.js
@@ -1,11 +1,11 @@
-import { dataComponentId, configurableComponent, resizableComponent, scaleOnResizeComponent } from '../common';
+import { dataComponentId, configurableComponent, resizableComponent, scaleOnResizeComponent, draggableComponent } from '../common';
 import { table } from './table';
 import { commontableid } from './ids';
 import extend from 'lodash/extend';
 
 const commontable = extend({}, table, {
     name: "Common ag-Grid",
-    html: `<div ${dataComponentId}="${commontableid}" style="width: 500px; height: 300px;" class="${configurableComponent} ${resizableComponent} ${scaleOnResizeComponent} ag-theme-blue"></div>`
+    html: `<div ${dataComponentId}="${commontableid}" style="width: 500px; height: 300px;" class="${configurableComponent} ${resizableComponent} ${draggableComponent} ${scaleOnResizeComponent} ag-theme-blue"></div>`
 });
 
 export default commontable;
