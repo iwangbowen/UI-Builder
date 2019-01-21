9053f986b20ac37387c9f54618ed7fe0ef1686f7
Bowen Wang <iwangbowen@gmail.com>
2019-01-21 17:11:46 +0800

    Add manual select draggable

--- src/components/@common/bootstrapmanualselectinput.js
+++ src/components/@common/bootstrapmanualselectinput.js
@@ -1,10 +1,10 @@
 import { bootstrapmanualselectinputid } from './ids';
-import { dataComponentId, formControl, formControlSm, defaultInputWidth } from '../common';
+import { dataComponentId, formControl, formControlSm, defaultInputWidth, draggableComponent } from '../common';
 import extend from 'lodash/extend';
 import manualselectinput from './manualselectinput';
 
 const bootstrapmanualselectinput = extend({}, manualselectinput, {
-    html: `<select ${dataComponentId}="${bootstrapmanualselectinputid}" class="${formControl} ${formControlSm}" style="width: ${defaultInputWidth};">
+    html: `<select ${dataComponentId}="${bootstrapmanualselectinputid}" class="${formControl} ${formControlSm} ${draggableComponent}" style="width: ${defaultInputWidth};">
                 <option value="value1">Text 1</option>
                 <option value="value2">Text 2</option>
            </select>`
