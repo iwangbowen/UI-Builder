f2bc1a26ce2a42fbd355b711f9ff0554858f75d2
Bowen Wang <iwangbowen@gmail.com>
2019-01-24 18:14:20 +0800

    Add image draggable

--- src/components/@common/image.js
+++ src/components/@common/image.js
@@ -1,5 +1,5 @@
 import { imageProperties as properties } from '../properties/image';
-import { configurableComponent } from '../common';
+import { configurableComponent, draggableComponent } from '../common';
 import { imagePlaceholder } from '../../common';
 
 const width = '135';
@@ -8,7 +8,7 @@ const height = '150';
 const image = {
     nodes: ['img'],
     name: 'Image',
-    html: `<img class="${configurableComponent}" src="${imagePlaceholder}" height="${height}" width="${width}">`,
+    html: `<img class="${configurableComponent} ${draggableComponent}" src="${imagePlaceholder}" height="${height}" width="${width}">`,
     width: `${width}px`,
     height: `${height}px`,
     image: "icons/image.svg",
